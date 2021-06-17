6ca2593ecc6236d1cb862976ea6a7537f662346d
Jasmine Graber <jasmine.n.graber@gmail.com>
2021-06-17 08:29:36 -0400

    Completed author table creation

--- database/book-tracker-table.sql
+++ database/book-tracker-table.sql
@@ -1,12 +1,19 @@
 BEGIN TRANSACTION;
 
+DROP TABLE IF EXISTS author;
+
 CREATE TABLE author (
 
-        author_id serial PRIMARY KEY NOT NULL,
+        author_id serial NOT NULL, -- constraint
         author_name varchar(100) NOT NULL,
         bipoc BOOLEAN DEFAULT FALSE,
-        race_ethnicity varchar(50), -- add constraint
-        gender varchar(20) NOT NULL -- add constraint
+        race_ethnicity varchar(50) NOT NULL, -- constraint
+        gender varchar(20) NOT NULL, -- constraint
+        
+        CONSTRAINT pk_author PRIMARY KEY (author_id),
+        CONSTRAINT CHK_race_ethnicity CHECK (race_ethnicity IN ('American Indian or Alaska Native', 'Black or African American', 'Hispanic or Latino', 'Middle Eastern', 'Native Hawaiian or Other Pacific Islander', 'White') ),
+        CONSTRAINT CHK_gender CHECK (gender IN ('Female', 'Male', 'Non-binary', 'Transgender'))
+
 );
 
 ROLLBACK;
